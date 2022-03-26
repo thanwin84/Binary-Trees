4122c9aab50ffa438cfece871a4256ada231df0a
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-26 21:27:48 +0600

    Create level order treversal.cpp

---
+++ treversal/level order treversal.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) {
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while (q.size() > 0) {
+            int levelSize = q.size();
+            // q size will be equal to current level size
+            vector<int> currentLevel;
+            // this will make sure that we loop level by level
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* current = q.front();
+                q.pop();
+                currentLevel.push_back(current->val);
+                if (current->left) {
+                    q.push(current->left);
+                }
+                if (current->right) {
+                    q.push(current->right);
+                }
+            }
+            // when we finished the loop, we already populated q with next level
+            result.push_back(currentLevel);
+        }
+        return result;
+    }
+};
