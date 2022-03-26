651f8efb747c2e1cd51f593cfbf1e663e12e31df
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-26 22:47:01 +0600

    Create preorder treversal.cpp

---
+++ treversal/cpp-solution/preorder treversal.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> result;
+    void solve(TreeNode * root){
+        if (!root){
+            return;
+        }
+        result.push_back(root->val);
+        solve(root->left);
+        solve(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        solve(root);
+        return result;
+    }
+};
+// iterative approach
+// time: O(n) and space O(height of the tree)
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root){
+            return result;
+        }
+        vector<TreeNode*> stack;
+        stack.push_back(root);
+        while (stack.size() > 0){
+            TreeNode * current = stack.back();
+            stack.pop_back();
+            result.push_back(current->val);
+            
+            // pushing right first, coz we need the to print the left first
+            if (current->right){
+                stack.push_back(current->right);
+            }
+            if (current->left){
+                stack.push_back(current->left);
+            }
+        }
+        return result;
+    }
+};
