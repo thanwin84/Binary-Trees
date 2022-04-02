eb9471b75d836816f517076bea53a7265c7a6728
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-02 12:53:31 +0600

    Update post order treversal.py

--- treversal/python solution/post order treversal.py
+++ treversal/python solution/post order treversal.py
@@ -1,4 +1,5 @@
 # time: O(n) and space: O(H of the tree)
+# left subtree -> right subtree -> process the root node 
 # using one stack
 class Solution:
     def postorderTraversal(self, root):
@@ -8,16 +9,17 @@ class Solution:
         stack = []
         current = root
         while current or stack:
-            # if current is not null, push it to the stack and to left
+            # if current is not null, push it to the stack and move left
             # cause we have to go left first, then right and then root
             if current:
                 stack.append(current)
                 current = current.left
             else:
+                # if current becomes null, that means we aleady treversed left subtree, now we have to check right subtree
                 # if stack top right is null, that means we have no right subtree to check
-                # we can simply pop the stack top
+                # we can process the root, STACK TOP IS THE ROOT NODE OF CURRENT SUBTREE
                 # after that, we will check again if popped item is the right subtree of stack top
-                # if popped item is right subtree stack top, that means we can pop stack top. cause after right subtree
+                # if popped item is right subtree stack top, cause we have go to root node after right subtree
                 # we print root
                 temp = stack[-1].right
                 if temp is None:
