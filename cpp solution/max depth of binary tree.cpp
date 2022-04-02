e90fd21dc73d83158de4cc6fa4066ceb7cebac9e
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-02 13:43:36 +0600

    Create max depth of binary tree.cpp

---
+++ cpp solution/max depth of binary tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  int maxDepht(TreeNode * root){
+    if (!root){
+      return 0;
+      }
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return 1 + max(left, right);
+  } 
+};
