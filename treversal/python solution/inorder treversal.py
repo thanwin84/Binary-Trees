7d6068f897e8508da88e0f0c32d5eb714875776f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-02 13:07:02 +0600

    Update inorder treversal.py

--- treversal/python solution/inorder treversal.py
+++ treversal/python solution/inorder treversal.py
@@ -1,5 +1,7 @@
 # time: O(n) and space O(height of the tree)
-# inspired from recursion
+# left -> root -> right
+# Alorithm: Go to left -> before going to left save the current node in stack cause we need to trace back to get the root node ->stack top is the root of current subtree
+# after processing root node go to right subtree
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
@@ -14,6 +16,7 @@ class Solution:
             else:
                 if not stack:
                     break
+                # stack top: root of the CURRENT subtree
                 topNode = stack.pop()
                 result.append(topNode.val)
                 current = topNode.right
