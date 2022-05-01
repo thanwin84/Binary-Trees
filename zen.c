2a64ba19046d733f7fcfc2258650731f2f22130b
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-05-01 22:59:21 +0600

    Create zen.c

---
+++ zen.c
@@ -0,0 +1,17 @@
+# there should be a class named adding. and you are just trying to print class properties
+fprintf(newrecord,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",
+      adding.account_number,
+      adding.client_name,
+      adding.client_dob.mm,
+      adding.client_dob.dd,
+      adding.client_dob.yy,
+      adding.client_age,
+      adding.client_address,
+      adding.client_citizenship,
+      adding.client_phone,
+      adding.client_account_type,
+      adding.client_amount,
+      adding.client_deposit.mm,
+      adding.client_deposit.dd,
+      adding.client_deposit.yy
+      );
