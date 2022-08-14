f0c7d24d23e5fb002a9410a122d49718fe7f554f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-08-14 14:19:37 +0600

    Update inorder treversal.cpp

--- treversal/cpp-solution/inorder treversal.cpp
+++ treversal/cpp-solution/inorder treversal.cpp
@@ -3,29 +3,32 @@
 // Alorithm: Go to left -> before going to left save the current node in stack cause we need to trace back to get the root node
 // stack top is the root of current subtree
 // after processing root node go to right subtree
+class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
-        if (!root){
-            return result;
-        }
-        TreeNode * current = root;
+        vector<int> inorder;
+        if (root == nullptr) return inorder;
         vector<TreeNode*> stack;
+        TreeNode* current = root;
         while (true){
             if (current){
                 stack.push_back(current);
+                // go to left subtree first
                 current = current->left;
             }
             else {
-                if (stack.size() == 0){
-                    break;
-                }
-                TreeNode * topNode = stack.back();
+                // if stack is empty, that means no need to process any node
+                if (stack.size() == 0) break;
+                // process the root node
+                TreeNode * root_node = stack.back();
                 stack.pop_back();
-                result.push_back(topNode->val);
-                current = topNode->right;
+                inorder.push_back(root_node->val);
+                // after processing root node, go to subtree
+                current = root_node->right;
+                
             }
+            
         }
-        return result;
+        return inorder;
     }
 };
