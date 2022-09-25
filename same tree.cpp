a51db4dc31e4062589aef585e9fac83905910fb4
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-09-25 23:45:50 +0600

    Update same tree.cpp

--- same tree.cpp
+++ same tree.cpp
@@ -1,17 +1,19 @@
+// time: O(n) and space: O(n) [ stack space]
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr and q == nullptr){
+    bool solve(TreeNode * p, TreeNode * q){
+        if (p == nullptr && q == nullptr){
             return true;
         }
         if (p == nullptr || q == nullptr){
             return false;
         }
-        if (p->val != q->val){
-            return false;
-        }
-        bool left = isSameTree(p->left, q->left);
-        bool right = isSameTree(p->right, q->right);
-        return left & right;
+        if (p->val != q->val) return false;
+        bool left = solve(p->left, q->left);
+        bool right = solve(p->right, q->right);
+        return left && right;
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return solve(p, q);
     }
 };
