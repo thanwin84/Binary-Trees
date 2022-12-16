d889fb79136dfda142f36853711de84050285c31
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-12-16 16:46:23 +0600

    Create is semmetric tree.cpp

---
+++ is semmetric tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSemmetricHelp(TreeNode * left, TreeNode * right){
+        if (left == nullptr || right == nullptr){
+            return left == right;
+        }
+        if (left->val != right->val) {
+            return false;
+        }
+        return isSemmetricHelp(left->left, right->right) && isSemmetricHelp(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return root == nullptr || isSemmetricHelp(root->left, root->right);
+    }
+};
+
+// time: O(n) and stack space is O(H)
